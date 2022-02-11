f97c185c3af3c57c527c8efc9759b8fd4cd6254e
liric <s-liric@bsd405.org>
2022-02-11 13:01:12 -0800

    commit

--- app/src/main/java/com/example/sevens/DisplayMessageActivity.kt
+++ app/src/main/java/com/example/sevens/DisplayMessageActivity.kt
@@ -2,6 +2,7 @@ package com.example.sevens
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class DisplayMessageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -9,6 +10,12 @@ class DisplayMessageActivity : AppCompatActivity() {
         setContentView(R.layout.activity_display_message)
     }
 
+    // Get the Intent that started this activity and extract the string
+    val message = intent.getStringExtra(EXTRA_MESSAGE)
 
+    // Capture the layout's TextView and set the string as its text
+    val textView = findViewById<TextView>(R.id.textView).apply {
+        text = message
+    }
 
 }
