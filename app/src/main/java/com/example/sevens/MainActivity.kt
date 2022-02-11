cc11a78c5d02c2c123e2dff17ca3c449f1afa535
liric <s-liric@bsd405.org>
2022-02-11 11:14:22 -0800

    Initial commit

---
+++ app/src/main/java/com/example/sevens/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.sevens
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+
+const val EXTRA_MESSAGE = "com.example.Sevens.MESSAGE"
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun sendMessage(view: View)
+    {
+        // do something in response to the button
+        val editText = findViewById<EditText>(R.id.editTextTextPersonName)
+        val message = editText.text.toString()
+        val intent = Intent(this, DisplayMessageActivity::class.java).apply {
+            putExtra(EXTRA_MESSAGE, message)
+        }
+        startActivity(intent)
+
+    }
+}
