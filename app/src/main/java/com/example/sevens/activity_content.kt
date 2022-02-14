bf82ee2509002d08cdf71d3fb0084efa36d38ab1
liric <s-liric@bsd405.org>
2022-02-14 13:44:27 -0800

    commit

---
+++ app/src/main/java/com/example/sevens/activity_content.kt
@@ -0,0 +1,13 @@
+package com.example.sevens
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class activity_content : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_content)
+    }
+
+
+}
