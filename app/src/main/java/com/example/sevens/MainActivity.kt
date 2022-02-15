7c30114dca7d917c08492cd47b790defea2f9eec
liric <s-liric@bsd405.org>
2022-02-15 13:58:18 -0800

    commit

--- app/src/main/java/com/example/sevens/MainActivity.kt
+++ app/src/main/java/com/example/sevens/MainActivity.kt
@@ -6,12 +6,15 @@ import android.os.Bundle
 import android.view.View
 import android.widget.EditText
 
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
 
-
+    fun sendMessage(view: View) {
+        // Do something in response to button click
+    }
 
 }
--- app/src/main/java/com/example/sevens/activity_content.kt
+++ app/src/main/java/com/example/sevens/activity_content.kt
@@ -7,7 +7,12 @@ class activity_content : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_content)
-    }
 
+        // Get the Intent that started this activity and extract the string
+
+
+        // Capture the layout's TextView and set the string as its text
+
+    }
 
 }
