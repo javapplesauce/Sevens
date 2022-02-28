68f0a4173bdcd00bd55b5952a671b40102fc9cde
liric <s-liric@bsd405.org>
2022-02-28 13:19:22 -0800

    commit

--- app/src/main/java/com/example/sevens/MainActivity.kt
+++ app/src/main/java/com/example/sevens/MainActivity.kt
@@ -1,20 +1,32 @@
 package com.example.sevens
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.AlarmClock.EXTRA_MESSAGE
 import android.view.View
+import android.widget.Button
 import android.widget.EditText
 
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+  
 
-    fun sendMessage(view: View) {
-        // Do something in response to button click
-    }
+
+
+
+
 
 }
+
+
+
+
+
+    
